98419d35d737b7c7ea0eb69cc118c49f2629a304
zhezhelocus <zhezhelocus@163.com>
2014-02-24 17:00:20 +0800

    淘女郎
    淘女郎页面

---
+++ taosir-0220/cont.php
@@ -0,0 +1,352 @@
+
+
+<link href="http://a.tbcdn.cn/tmse/1/dpl/??sir-activity/v18/modules/sir-header/sir-header.css" rel="stylesheet">
+<style>
+  .sir-header {
+width: 100%;
+height: 80px;
+background: #323232;
+}
+</style>
+<div class="sir-header">
+    <div class="sir-global clearfix">
+       <a href="http://man.taobao.com/" class="sir-logo" title="淘先生" target="_blank"></a>
+       <a href="http://man.taobao.com/user/register.htm" class="sir-join" target="_blank">+ 加入先生</a>
+    </div>
+</div>
+
+
+<!--主页面内容开始-->
+
+<style type="text/css">
+
+#page{
+    width: 100%;
+}
+.wrap-page{
+    width: 100%;
+    min-width: 990px;
+    background-color: #fff;
+}
+.wrap-page .hd{
+    width: 100%;
+    height: 384px;
+    background: url(http://gtms01.alicdn.com/tps/i1/T14s9dFp4aXXXprv_A-1920-384.png) no-repeat center 0; 
+}
+.w990{
+    width: 990px;
+    height: 100%;
+    margin: 0 auto;
+    font-size: 0 ;
+    line-height: 0;
+    position: relative;
+    overflow: hidden;
+}
+
+.item1{
+    width: 100%;
+    height: 343px;
+    background: url(http://gtms04.alicdn.com/tps/i4/T10eKaFu0cXXXK2vDA-1920-343.png) no-repeat center 0;
+}
+.item1 .w990 a.a-pk{
+    display: block;
+    width: 105px;
+    height: 128px;
+    position: absolute;
+    right: 25px;
+    top: 100px;
+    cursor: pointer;
+    z-index: 1000;
+}
+.item1 .w990 a.a-hover{
+    display: block;
+    width: 391px;
+    height: 343px;
+    position: absolute;
+    right: 233px;
+    top: 0px;
+    z-index: 1000;
+    cursor: pointer;
+}
+.item1 .w990 a.a-hover:hover{
+    background: url(http://gtms01.alicdn.com/tps/i1/T1V9icFvBfXXbYNnZg-391-343.jpg) no-repeat;
+}
+.item2{
+    width: 100%;
+    height: 347px;
+    background: url(http://gtms02.alicdn.com/tps/i2/T1snKbFypbXXca32HA-1920-347.png) no-repeat center 0;
+}
+.item2 .w990 a.a-pk{
+    display: block;
+    width: 105px;
+    height: 128px;
+    position: absolute;
+    right: 25px;
+    top: 110px;
+    cursor: pointer;
+    z-index: 1000;
+}
+.item2 .w990 a.a-hover{
+    display: block;
+    width: 366px;
+    height: 347px;
+    position: absolute;
+    left: 0;
+    top: 0px;
+    z-index: 1000;
+    cursor: pointer;
+}
+.item2 .w990 a.a-hover:hover{
+    background: url(http://gtms04.alicdn.com/tps/i4/T19u9cFrRbXXbfTMfG-366-347.png) no-repeat;
+}
+.item3{
+    width: 100%;
+    height: 354px;
+    background: url(http://gtms03.alicdn.com/tps/i3/T1Iz1cFrFbXXaC5LLA-1920-354.png) no-repeat center 0;
+}
+.item3 .w990 a.a-pk{
+    display: block;
+    width: 105px;
+    height: 128px;
+    position: absolute;
+    right: 25px;
+    top: 110px;
+    cursor: pointer;
+    z-index: 1000;
+}
+.item3 .w990 a.a-hover{
+    display: block;
+    width: 284px;
+    height: 354px;
+    position: absolute;
+    right: 189px;
+    top: 0px;
+    z-index: 1000;
+    cursor: pointer;
+}
+.item3 .w990 a.a-hover:hover{
+    background: url(http://gtms04.alicdn.com/tps/i4/T16xecFtRbXXcEf3sh-284-354.png) no-repeat;
+}
+.item4{
+    width: 100%;
+    height: 370px;
+    background: url(http://gtms03.alicdn.com/tps/i3/T1m45dFqRaXXcICLYA-1920-370.png) no-repeat center 0;
+}
+.item4 .w990 a.a-pk{
+    display: block;
+    width: 105px;
+    height: 128px;
+    position: absolute;
+    right: 25px;
+    top: 110px;
+    cursor: pointer;
+    z-index: 1000;
+}
+.item4 .w990 a.a-hover{
+    display: block;
+    width: 348px;
+    height: 370px;
+    position: absolute;
+    left: 0;
+    top: 0px;
+    z-index: 1000;
+    cursor: pointer;
+}
+.item4 .w990 a.a-hover:hover{
+    background: url(http://gtms04.alicdn.com/tps/i4/T1wrmbFxXgXXatvOHd-348-370.png) no-repeat;
+}
+
+
+/*
+ *  导航
+ */
+.nav{
+    width: 16px;
+    position: fixed;
+    top:150px;
+    margin-left: 515px;
+    left:50%;
+    z-index: 1000;
+    _position: absolute;
+    _top:200px;
+    _right:10px;
+
+}
+.nav ul li{
+    float: left;
+    width: 16px;
+    height: 16px;
+    margin-bottom: 10px;
+}
+
+.nav ul li a{
+    width: 10px;
+    height: 10px;
+    display: block;
+    border-radius: 5px;
+    background-color: transparent;
+    border:1px solid #d0a35a;
+}
+.nav ul li a.current{
+    width: 12px;
+    height: 12px;
+    border-radius: 6px;
+    background-color: #d0a35a;
+}
+
+/*
+ *  banner
+ */
+.width990{
+    width: 990px;
+    margin:0 auto;
+    font-size: 0;
+    line-height: 0;
+    overflow: hidden;
+}
+ul.banner{
+    width: 1010px;
+}
+ul.banner li{
+    width: 230px;
+    height: 160px;
+    float: left;
+    margin: 0 20px 20px 0;
+    _display:inline;
+}
+</style>
+
+<div class="wrap-page">
+    <div class="hd">
+        <div class="w990">
+            <img src="http://gtms02.alicdn.com/tps/i2/T10MeeFthXXXcF.kfh-990-384.jpg" />
+        </div>
+    </div>
+
+    <div class="item1" id="f1">
+        <div class="w990">
+            <img src="http://gtms03.alicdn.com/tps/i3/T1TdWdFpJaXXbCKjPh-990-343.png" />
+            <cms:custom group="干练型" title="干练型" fields="linkpk:pk链接:url,linkhover:爱此款链接:url" name="干练型">
+                <a href="$!linkpk" target="_blank" class="a-pk"></a>
+                <a href="$!linkhover" target="_blank" class="a-hover"></a>
+            </cms:custom>
+        </div>
+    </div>
+
+    <div class="item2" id="f2">
+        <div class="w990">
+            <img src="http://gtms03.alicdn.com/tps/i3/T12s5cFvhbXXX5L6Th-990-347.png" />
+            <cms:custom group="精悍型" title="精悍型" fields="linkpk:pk链接:url,linkhover:爱此款链接:url" name="精悍型">
+                <a href="$!linkpk" target="_blank" class="a-pk"></a>
+                <a href="$!linkhover" target="_blank" class="a-hover"></a>
+            </cms:custom>
+        </div>
+    </div>
+
+    <div class="item3" id="f3">
+        <div class="w990">
+            <img src="http://gtms04.alicdn.com/tps/i4/T1DwqeFqxXXXcuNzYh-990-354.png" />
+            <cms:custom group="厚实型" title="厚实型" fields="linkpk:pk链接:url,linkhover:爱此款链接:url" name="厚实型">
+                <a href="$!linkpk" target="_blank" class="a-pk"></a>
+                <a href="$!linkhover" target="_blank" class="a-hover"></a>
+            </cms:custom>
+        </div>
+    </div>
+
+    <div class="item4" id="f4">
+        <div class="w990">
+            <img src="http://gtms01.alicdn.com/tps/i1/T11AydFpXaXXaAlP_h-990-370.png" />
+            <cms:custom group="岩石型" title="岩石型" fields="linkpk:pk链接:url,linkhover:爱此款链接:url" name="岩石型">
+                <a href="$!linkpk" target="_blank" class="a-pk"></a>
+                <a href="$!linkhover" target="_blank" class="a-hover"></a>
+            </cms:custom>
+        </div>
+    </div>
+
+    <div class="width990">
+        <ul class="clearfix banner">
+            <cms:custom group="banner" title="banner" defaultRow="4" row="12" fields="link:链接:url,image:图片230x160:img" name="banner">
+                #foreach($item in $customList)
+                    <li>
+                        <a href="$!item.link" target="_blank">
+                            <img data-ks-lazyload="$!item.image" width=230 height=160 />
+                        </a>
+                    </li>
+                #end
+            </cms:custom>
+        </ul>
+    </div>
+
+</div>
+<div class="nav" id="J_rightNav">
+    <ul class="clearfix navlist">
+        <li>
+            <a target="_self" href="#f1" class="f1"></a>
+        </li>
+        <li>
+            <a target="_self" href="#f2" class="f2"></a>
+        </li>
+        <li>
+            <a target="_self" href="#f3" class="f3"></a>
+        </li>
+        <li>
+            <a target="_self" href="#f4" class="f4"></a>
+        </li>
+    </ul>
+</div>
+
+
+<script type="text/javascript">
+
+KISSY.use('core, sizzle, event',function(S, Event){
+   	var $ = KISSY.all;
+    //导航
+    var eTop = $('#J_rightNav').offset().top;  //150px
+              
+
+    var nav = $('ul.navlist a');
+
+
+    $(window).on('scroll',function(){
+
+        var top = document.body.scrollTop || document.documentElement.scrollTop;
+            
+        var len = $('.navlist li').length;
+        for(var i = 1; i <= len; i++) {
+            var $id = $('#f' + i),
+                $cls = $('.f' + i);
+              //  console.log($id);
+       // console.log(top);
+
+            if(top >= $id.offset().top-150){
+                nav.removeClass('current');
+                $cls.addClass('current');
+            }else{
+                $cls.removeClass('current');
+            }
+        }
+    });
+
+});
+
+
+//懒加载
+function showImg(){
+     KISSY.use('datalazyload',function(){
+          var lazy = new KISSY.DataLazyload({
+               
+               diff:200
+
+          });
+     })
+}
+
+KISSY.ready(function(){
+     
+     showImg();
+     
+});
+</script>
+
+
+<!--主页面内容结束-->
